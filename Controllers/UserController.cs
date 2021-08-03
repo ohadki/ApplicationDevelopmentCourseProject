f4b30259975d55a767ca23f39bb83d4d693d095f
ohadki <ohadozcohen@gmail.com>
2021-08-03 17:58:27 +0200

    Added a user model, login page and UserController. Login function can get to the controller but there is no DB yet.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationDevelopmentCourseProject.Models;
+
+namespace ApplicationDevelopmentCourseProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CheckValidUser(User model)
+        {
+            string result = "Fail";
+            var DataItem = "Test"; //TODO: LINQ
+            if (DataItem != null)
+            {
+                //TODO: Handle valid user
+            }
+            return Json(result);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentCourseProject.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Id { get; set; }
+    }
+}
