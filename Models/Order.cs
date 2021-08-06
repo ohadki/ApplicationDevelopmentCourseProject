c8c0a27d2b6dd185d3c23ab88ad4783d8d13ca94
TalYacobi <TalYacobi@TAL>
2021-08-06 19:09:44 +0300

    Added new order model. Added new order details model. Added new application user model.

---
+++ Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentCourseProject.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public IEnumerable<OrderDetails> OrderLines { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public decimal OrderTotal { get; set; }
+
+        public DateTime OrderPlaced { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
