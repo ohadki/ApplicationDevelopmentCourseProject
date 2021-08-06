5b08584d9c46e670a2184c258da1ff2ff83d4b06
TalYacobi <TalYacobi@TAL>
2021-08-06 19:00:21 +0300

    Changed property name

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,7 +13,7 @@ namespace ApplicationDevelopmentCourseProject.Models
         public string LongDescription { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public bool IsPreferedFood { get; set; }
+        public bool IsPreferedProduct { get; set; }
         public int InStock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
