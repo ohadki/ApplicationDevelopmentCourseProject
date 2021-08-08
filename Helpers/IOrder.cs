8c301e78abbc88362c7f8752086c33144a51b062
TalYacobi <TalYacobi@TAL>
2021-08-08 20:37:22 +0300

    Added interface for Order class.

---
+++ Helpers/IOrder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApplicationDevelopmentCourseProject.Enums;
+using ApplicationDevelopmentCourseProject.Models;
+
+namespace ApplicationDevelopmentCourseProject.Helpers
+{
+	public interface IOrder
+	{
+		void CreateOrder(Order order);
+		Order GetById(int orderId);
+		IEnumerable<Order> GetByUserId(string userId);
+		IEnumerable<Order> GetAll();
+		IEnumerable<Order> GetUserLatestOrders(int count, string userId);
+		IEnumerable<Product> GetUserMostPopularFoods(string id);
+		IEnumerable<Order> GetFilteredOrders(
+			string userId = null,
+			OrderBy orderBy = OrderBy.None,
+			int offset = 0,
+			int limit = 10,
+			decimal? minimalPrice = null,
+			decimal? maximalPrice = null,
+			DateTime? minDate = null,
+			DateTime? maxDate = null,
+			string zipCode = null
+			);
+	}
+}
+
+
+
+
+namespace Shop.Data
+{
+	
+}
