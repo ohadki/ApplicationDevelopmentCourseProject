cd6c5e83d17a9fd1f4f577da0db216f05b4a50bd
TalYacobi <TalYacobi@TAL>
2021-08-08 20:37:11 +0300

    Added interface for Category class.

---
+++ Helpers/ICategory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ApplicationDevelopmentCourseProject.Models;
+
+namespace ApplicationDevelopmentCourseProject.Helpers
+{
+    public interface ICategory
+    {
+        IEnumerable<Category> GetAll();
+        Category GetById(int id);
+        void NewCategory(Category category);
+        void EditCategory(Category category);
+        void DeleteCategory(int id);
+    }
+}
