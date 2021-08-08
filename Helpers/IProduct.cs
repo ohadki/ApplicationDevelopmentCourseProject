503da21422599c16a6b61166dccc08fd11501b92
TalYacobi <TalYacobi@TAL>
2021-08-08 20:36:59 +0300

    Added interface for product class.

---
+++ Helpers/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ApplicationDevelopmentCourseProject.Models;
+
+namespace ApplicationDevelopmentCourseProject.Helpers
+{
+    public interface IProduct
+    {
+        IEnumerable<Product> GetAll();
+        IEnumerable<Product> GetPreferred(int count);
+        IEnumerable<Product> GetProductsByCategoryId(int categoryId);
+        IEnumerable<Product> GetFilteredProducts(int id, string searchQuery);
+        IEnumerable<Product> GetFilteredProducts(string searchQuery);
+        Product GetById(int id);
+        void NewProduct(Product Product);
+        void EditProduct(Product Product);
+        void DeleteProduct(int id);
+    }
+}
