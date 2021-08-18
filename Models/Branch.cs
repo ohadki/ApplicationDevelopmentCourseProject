936af87b070759b4f755e67cbc942d3ae4bfe31a
talyacobiz <talyacobi123@gmail.com>
2021-08-18 18:15:19 +0300

    Create Branch.cs
    Added new branch class.

---
+++ Models/Branch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentCourseProject.Models
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+        //TODO: Think if we want to show the product for each branch.
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
