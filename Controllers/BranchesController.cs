be58ecce7ea27ff5f1d26a0d9c72581ca13977d1
talyacobiz <talyacobi123@gmail.com>
2021-08-19 18:15:19 +0300

    Fix Controller
    Fix branch controller after adding new members in branch class.

--- Controllers/BranchesController.cs
+++ Controllers/BranchesController.cs
@@ -54,7 +54,7 @@ namespace ApplicationDevelopmentCourseProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Location,ImageUrl")] Branch branch)
+        public async Task<IActionResult> Create([Bind("Id,BranchName,Address,XCoordinate,YCoordinate,ImageUrl")] Branch branch)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace ApplicationDevelopmentCourseProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Location,ImageUrl")] Branch branch)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BranchName,Address,XCoordinate,YCoordinate,ImageUrl")] Branch branch)
         {
             if (id != branch.Id)
             {
