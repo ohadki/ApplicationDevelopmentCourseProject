fd9b8a8c5bc21bda4fb33543b5e4d47969265e41
talyacobiz <talyacobi123@gmail.com>
2021-08-19 19:16:55 +0300

    Contact Us
    Added Contact us page

--- Migrations/20210819161228_added-branches.Designer.cs
+++ Migrations/20210819161228_added-branches.Designer.cs
@@ -4,14 +4,16 @@ using ApplicationDevelopmentCourseProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApplicationDevelopmentCourseProject.Migrations
 {
     [DbContext(typeof(ApplicationDevelopmentCourseProjectContext))]
-    partial class ApplicationDevelopmentCourseProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20210819161228_added-branches")]
+    partial class addedbranches
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
