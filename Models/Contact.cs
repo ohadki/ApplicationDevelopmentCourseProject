fa28b786421bbfb8b17ab4206394f152b86d91ef
talyacobiz <talyacobi123@gmail.com>
2021-08-25 23:16:54 +0300

    Added data annotations

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -10,9 +10,24 @@ namespace ApplicationDevelopmentCourseProject.Models
     public class Contact
     {
         public int Id { get; set; }
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(150, MinimumLength = 1)]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Your contact number :")]
+        [Required(ErrorMessage = "A phone number is required.")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone Number")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Phone Number.")]
         public string Telephone { get; set; }
+        [DisplayName("Message")]
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(250, MinimumLength = 1)]
+        [DataType(DataType.Text)]
         public string Message { get; set; }
     }
 }
