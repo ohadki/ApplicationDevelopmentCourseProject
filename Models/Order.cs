97c55d8121b79e49c8260a0599da3dd7cf34a3e0
talyacobiz <talyacobi123@gmail.com>
2021-09-05 19:51:43 +0300

    Changed order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -10,34 +10,12 @@ namespace ApplicationDevelopmentCourseProject.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "ZipCode is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "ZipCode should be minimum 3 characters and a maximum of 50 characters")]
-
-        public string ZipCode { get; set; }
-
-        [Required(ErrorMessage = "Address is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "Address should be minimum 3 characters and a maximum of 50 characters")]
-        [DataType(DataType.Text)]
-        [Display(Name = "Address")]
-        public string Address { get; set; }
-
-        [Required(ErrorMessage = "City is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "City should be minimum 3 characters and a maximum of 50 characters")]
-        [DataType(DataType.Text)]
-        public string City { get; set; }
-
-        [Required(ErrorMessage = "Country is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "Country should be minimum 3 characters and a maximum of 50 characters")]
-        [DataType(DataType.Text)]
-        public string Country { get; set; }
 
         public decimal OrderTotal { get; set; }
 
         public DateTime OrderPlaced { get; set; }
+
+        public IEnumerable<CartItem> Products { get; set; }
 
         public string UserId { get; set; }
     }
